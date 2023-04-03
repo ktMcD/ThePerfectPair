a942cf80715400b3d4a4ba04cc2c6f3aeadab7a2
ktMcD <30326066+ktMcD@users.noreply.github.com>
2023-04-03 14:50:08 -0700

    Revert "Individual pages (#17) (#19)" (#20)
    This reverts commit 5d3c50814b9a6b1ae837439e75e41d9a85ea3d07.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PairingListComponent } from './food-wine-pairing/pairing-list/pairing-list.component';
-import { RandomPairingListComponent } from './random-pairings/random-pairing-list/random-pairing-list.component';
-import { WelcomePageComponent } from './welcome/welcome-page/welcome-page.component';
 import { WinePairingListComponent } from './wine-food-pairing/wine-pairing-list/wine-pairing-list.component';
 
 const routes: Routes = [
-{path: "", component:WelcomePageComponent},
-{path: 'food-wine-pairing', component:PairingListComponent},
-{path: 'wine-food-pairing', component:WinePairingListComponent},
-{path: 'random-pairing', component:RandomPairingListComponent}
-
+{path: 'food', component:PairingListComponent},
+{path: 'wine-pairing-list', component:WinePairingListComponent}
 ];
 
 @NgModule({
